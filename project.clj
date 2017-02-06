e51d61b9fb5d84ed200caaea1539fa32d522d153
mveritym <mveritym@gmail.com>
2017-02-06 10:22:26 +0000

    Adding clojurescript

--- project.clj
+++ project.clj
@@ -4,12 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.456"]
                  [twitter-api "0.7.9"]
                  [environ "1.1.0"]
                  [com.cemerick/url "0.1.1"]]
   :main ^:skip-aot markov-bot.core
   :target-path "target/%s"
-  :plugins [[lein-environ "1.1.0"]]
+  :plugins [[lein-environ "1.1.0"]
+            [lein-cljsbuild "1.1.5"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-to "target/cljsbuild-main.js"
+                                   :pretty-print true}}]}
   :profiles {:uberjar {:aot :all}
              :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]]
                    :env {:consumer-key "Kkhxl0qZ2AzaFEc6WXa5bDD8L"
